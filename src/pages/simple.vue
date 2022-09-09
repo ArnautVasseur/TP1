13f3cf7983089e49e148d6f9d24c3c73d8ede1d0
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-09-09 08:35:01 +0200

    Utilize v-bind in simple.vue

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import Card from "../components/card.vue"
+const unObjet = {
+    nom:"Beverly Hills",
+    prix:12000,
+    favori:true,
+    nbrSDB:3
+};
 
 </script>
 
 <template>
     <section>
-        <Card></Card>
+        <Card v-bind="unObjet"></Card>
     </section>
 </template>
